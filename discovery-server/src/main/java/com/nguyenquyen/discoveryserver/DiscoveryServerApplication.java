d1f6f9a3f7b413d5014b396e265c03f44ece9b79
quyennguyencoder <quyenn0301@ut.edu.vn>
2026-06-07 15:15:08 +0700

    initial configuration

--- discovery-server/src/main/java/com/nguyenquyen/discoveryserver/DiscoveryServerApplication.java
+++ discovery-server/src/main/java/com/nguyenquyen/discoveryserver/DiscoveryServerApplication.java
@@ -4,11 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableEurekaServer
 @SpringBootApplication
+@EnableEurekaServer
 public class DiscoveryServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DiscoveryServerApplication.class, args);
     }
+
 }
--- init-db.sql
+++
@@ -1,3 +0,0 @@
-CREATE DATABASE auth_service;
-CREATE DATABASE user_service;
-CREATE DATABASE friend_service;
